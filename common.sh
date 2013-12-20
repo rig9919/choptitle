f57b0ce587fbe161663cf824a3caedda8019efef
Kyle <rig9919hd@gmail.com>
2013-12-20 11:57:58 -0600

    added possibility for future use of radial comparisons. moved list_iframes to common.

--- common.sh
+++ common.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
-VERSION="0.3"
+VERSION="0.4"
+
+list_iframes() {
+    ffmpeg -i "$1" -vf select='eq(pict_type\,I)' -vsync 2 -f image2 "$tmp_path/%05d.bmp" -loglevel debug 2>&1 | awk '{ if ($10 == "pict_type:I" || $9 == "pict_type:I") print substr($6,3); fflush(stdout)}'
+}
 
 convertsecs() {
     h=$(echo "$1/3600" | bc)
--- hashcompare.py
+++ hashcompare.py
@@ -14,9 +14,19 @@ def compare(file1, file2):
     hash2 = pHash.imagehash(file2)
     return pHash.hamming_distance(hash1, hash2)
 
+def radial_compare(file1, file2):
+    digest1 = pHash.image_digest(file1)
+    digest2 = pHash.image_digest(file2)
+    cc = pHash.crosscorr(digest1, digest2)[1]
+    if cc == cc:
+        return int(round(cc * -100))
+    else:
+        return 0
+
 if __name__=='__main__':
     import sys
     file1 = sys.argv[1]
     for f in sys.argv[2:]:
-        print f + ': ' + str(compare(file1, f))
+        print f + ' (hash): ' + str(compare(file1, f))
+        print f + ' (digest): ' + str(radial_compare(file1, f))
     #print compare(file1, file2)
