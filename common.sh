1f8f5567a996638f6e1304cbf3be6f906f9b026e
Kyle <rig9919hd@gmail.com>
2013-12-21 22:06:00 -0600

    made common.sh compatible with new script

--- common.sh
+++ common.sh
@@ -2,7 +2,11 @@
 VERSION="0.4"
 
 list_iframes() {
-    ffmpeg -i "$1" -vf select='eq(pict_type\,I)' -vsync 2 -f image2 "$2%05d.bmp" -loglevel debug 2>&1 | awk '{ if ($10 == "pict_type:I" || $9 == "pict_type:I") print substr($6,3); fflush(stdout)}'
+	i=1
+    ffmpeg -i "$1" -to $3 -vf select='eq(pict_type\,I)' -vsync 2 -f image2 "$2/%05d.bmp" -loglevel debug 2>&1 | awk '{ if ($10 == "pict_type:I" || $9 == "pict_type:I") print substr($6,3); fflush(stdout)}' | while read timestamp; do
+		printf "%05d:%s\n" $i $timestamp
+		let i=i+1
+	done
 }
 
 convertsecs() {
